5dbcbcd91356c86cbf4659e3459acf1154343e95
alvcalgon <alvcalgon	@alum.us.es>
2018-10-05 21:36:49 +0200

    populateDatabase completado y Actor::userAccount

--- Acme-Shout/src/main/java/domain/Actor.java
+++ Acme-Shout/src/main/java/domain/Actor.java
@@ -1,7 +1,13 @@
 
 package domain;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import security.UserAccount;
 
 @Entity
 public class Actor extends DomainEntity {
@@ -12,8 +18,22 @@ public class Actor extends DomainEntity {
 		super();
 	}
 
+
 	// Attributes
 
 	// Relationships
+	private UserAccount	userAccount;
+
+
+	@NotNull
+	@Valid
+	@OneToOne(cascade = CascadeType.ALL, optional = false)
+	public UserAccount getUserAccount() {
+		return this.userAccount;
+	}
+
+	public void setUserAccount(final UserAccount userAccount) {
+		this.userAccount = userAccount;
+	}
 
 }
