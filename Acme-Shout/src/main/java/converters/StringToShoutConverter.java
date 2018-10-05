1a19583fc7233e8c8670abfef27fae9d7601e3be
julgargal <juliagarciagallego@gmail.com>
2018-10-05 23:44:22 +0200

    Convertidores

---
+++ Acme-Shout/src/main/java/converters/StringToShoutConverter.java
@@ -0,0 +1,43 @@
+/*
+ * StringToAnswerConverter.java
+ * 
+ * Copyright (C) 2017 Universidad de Sevilla
+ * 
+ * The use of this project is hereby constrained to the conditions of the
+ * TDG Licence, a copy of which you may download from
+ * http://www.tdg-seville.info/License.html
+ */
+
+package converters;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import domain.Shout;
+
+@Component
+@Transactional
+public class StringToShoutConverter implements Converter<String, Shout> {
+
+	@Autowired
+	ShoutRepository	shoutRepository;
+
+
+	@Override
+	public Shout convert(final String text) {
+		Shout result;
+		int id;
+
+		try {
+			id = Integer.valueOf(text);
+			result = this.shoutRepository.findOne(id);
+		} catch (final Throwable oops) {
+			throw new IllegalArgumentException(oops);
+		}
+
+		return result;
+	}
+
+}
