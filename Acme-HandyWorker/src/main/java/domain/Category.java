ec914df1b535f988e9d5d4cd85aeea342280f033
marjimveg1 <36474784+marjimveg1@users.noreply.github.com>
2018-11-07 09:45:08 +0100

    Category/CategoryTranslation Implementado

--- Acme-HandyWorker/src/main/java/domain/Category.java
+++ Acme-HandyWorker/src/main/java/domain/Category.java
@@ -11,8 +11,6 @@ import javax.persistence.OneToMany;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
-import org.hibernate.validator.constraints.NotBlank;
-
 @Entity
 @Access(AccessType.PROPERTY)
 public class Category extends DomainEntity {
@@ -26,34 +24,22 @@ public class Category extends DomainEntity {
 
 	// Attributes ----------------------------------------------------------------------
 
-	private String	name;
-	private String	language;
-
-
-	@NotBlank
-	public String getName() {
-		return this.name;
-	}
-
-	public void setName(final String name) {
-		this.name = name;
-	}
-
-	@NotBlank
-	public String getLanguage() {
-		return this.language;
-	}
-
-	public void setLanguage(final String language) {
-		this.language = language;
-	}
-
-
 	// Relationships ----------------------------------------------------------
 
-	private Category				parent;
-	private Collection<Category>	descendants;
+	private Category						parent;
+	private Collection<Category>			descendants;
+	private Collection<CategoryTranslation>	categoriesTranslations;
 
+
+	@NotNull
+	@Valid
+	@OneToMany
+	public Collection<CategoryTranslation> getCategoriesTranslations() {
+		return this.categoriesTranslations;
+	}
+	public void setCategoriesTranslations(final Collection<CategoryTranslation> categoriesTranslations) {
+		this.categoriesTranslations = categoriesTranslations;
+	}
 
 	@Valid
 	@ManyToOne(optional = true)
