d291a8ecb616bb1889cf7ee7a4db5ce5531dc128
marjimveg1 <36474784+marjimveg1@users.noreply.github.com>
2018-11-16 16:50:08 +0100

    Update ApplicationRepository.java

--- Acme-HandyWorker/src/main/java/repositories/ApplicationRepository.java
+++ Acme-HandyWorker/src/main/java/repositories/ApplicationRepository.java
@@ -1,6 +1,9 @@
 package repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public class ApplicationRepository {
+@Repository
+public interface ApplicationRepository extends JpaRepository<domain.Application, int>{
 
 }
