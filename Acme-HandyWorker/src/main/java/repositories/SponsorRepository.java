131b9256cd991b7158b6eb3e2ac386a022b9ba93
marjimveg1 <36474784+marjimveg1@users.noreply.github.com>
2018-11-16 20:54:56 +0100

    CRUD

--- Acme-HandyWorker/src/main/java/repositories/SponsorRepository.java
+++ Acme-HandyWorker/src/main/java/repositories/SponsorRepository.java
@@ -4,7 +4,9 @@ package repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import domain.Sponsor;
+
 @Repository
-public interface SponsorRepository extends JpaRepository<SponsorRepository, Integer> {
+public interface SponsorRepository extends JpaRepository<Sponsor, Integer> {
 
 }
--- Acme-HandyWorker/src/main/java/services/SponsorService.java
+++ Acme-HandyWorker/src/main/java/services/SponsorService.java
@@ -1,12 +1,16 @@
 
 package services;
 
+import java.util.Collection;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import repositories.SponsorRepository;
+import domain.Sponsor;
 
 @Service
 @Transactional
@@ -25,6 +29,49 @@ public class SponsorService {
 	}
 	//Simple CRUD methods -------------------------------------------
 
+	public Sponsor create() {
+		Sponsor result;
+
+		result = new Sponsor();
+
+		return result;
+	}
+
+	public Sponsor save(final Sponsor sponsor) {
+		Assert.notNull(sponsor);
+
+		Sponsor result;
+
+		result = this.sponsorRepository.save(sponsor);
+
+		return result;
+	}
+
+	public Sponsor findOne(final int idSponsor) {
+		Sponsor result;
+
+		Assert.isTrue(idSponsor != 0);
+
+		result = this.sponsorRepository.findOne(idSponsor);
+
+		return result;
+	}
+	public Collection<Sponsor> findAll() {
+		Collection<Sponsor> result;
+
+		result = this.sponsorRepository.findAll();
+
+		Assert.notNull(result);
+
+		return result;
+	}
+	public void delete(final Sponsor sponsor) {
+		Assert.isTrue(sponsor.getId() != 0);
+		Assert.notNull(sponsor);
+		Assert.isTrue(this.sponsorRepository.exists(sponsor.getId()));
+
+		this.sponsorRepository.delete(sponsor);
+	}
 	//Other business methods-------------------------------------------
 
 }
